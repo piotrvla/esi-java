5b66563c46248d6fac2a638843bf6cc088bc34ce
56212 <56212@etu.he2b.be>
2022-04-18 20:37:07 +0200

    stib view and configmanager

---
+++ Stib/src/main/java/g56212/atlg4/stib/StibRide.java
@@ -0,0 +1,24 @@
+package g56212.atlg4.stib;
+
+import g56212.atlg4.stib.config.ConfigManager;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StibRide extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StibRide.class.getResource("stib-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 768);
+        stage.setTitle("G56212 - STIB Route");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Stib/src/main/java/g56212/atlg4/stib/utils/observer/Observable.java
@@ -0,0 +1,25 @@
+package g56212.atlg4.stib.utils.observer;
+
+public interface Observable {
+
+    /**
+     * Adds an observer to the list of observers.
+     *
+     * @param observer The observer to be added.
+     */
+    void addObserver(Observer observer);
+
+    /**
+     * Removes the observer from the list.
+     *
+     * @param observer The  observer to be removed.
+     */
+    void removeObserver(Observer observer);
+
+    /**
+     * Notifies all observers by calling their 'update' method.
+     */
+    void notifyObservers(Object args);
+
+
+}
